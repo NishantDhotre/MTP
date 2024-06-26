3a68e3cf50a5dc920f4e1a31f34731a9cc2994d8
nishantdhotre <dhotrenishant@gmail.com>
2024-06-27 01:14:04 +0530

    converting classification to rigression

--- job-new.sh
+++ job-new.sh
@@ -5,11 +5,11 @@
 #SBATCH --mem=30gb                   # Job memory request
 #SBATCH --partition=gpupart_p100          # GPU Partition
 #SBATCH --gpus-per-node=2            #number of GPUS
-#SBATCH --time=0-23:59:00            # Time limit hrs:min:sec
-#SBATCH --output=/home/m1/23CS60R48/MTP/output_%j.log    # Standard output and error log
+#SBATCH --time=1-23:00:00            # Time limit hrs:min:sec
+#SBATCH --output=/home/m1/23CS60R48/MTP/UNETsWIN2_output_%j.log    # Standard output and error log
 
 #module load anaconda3
 conda init
 conda activate gpu
-python3 unetSwin.py
+python3 UNETsWIN2.py
 # this is workling!!!
