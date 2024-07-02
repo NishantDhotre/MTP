d34094edeb7e3d8a93549b7b5c1f55d0e3916757
nishantdhotre <dhotrenishant@gmail.com>
2024-07-02 12:27:37 +0530

    updates

--- job-new.sh
+++ job-new.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH --job-name=unetSwin_job_4      # Job name
-#SBATCH --nodes=1                    # Run all processes on a single node
-#SBATCH --ntasks=1                   # Run a single task
+#SBATCH --nodes=2                    # Run all processes on a single node
+#SBATCH --ntasks=2                   # Run a single task
 #SBATCH --mem=30gb                   # Job memory request
 #SBATCH --partition=gpupart_p100          # GPU Partition
 #SBATCH --gpus-per-node=2            #number of GPUS
 #SBATCH --time=1-23:00:00            # Time limit hrs:min:sec
-#SBATCH --output=/home/m1/23CS60R48/MTP/unetSwin_9_flair_t1ce_t2_%j.log    # Standard output and error log
+#SBATCH --output=/home/m1/23CS60R48/MTP/unetSwin_test_%j.log    # Standard output and error log
 
 #module load anaconda3
 conda init
