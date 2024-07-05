3292facba1c68356a040e7e418b374935aa17182
nishantdhotre <dhotrenishant@gmail.com>
2024-07-05 20:26:33 +0530

    updates

--- job-new.sh
+++ job-new.sh
@@ -3,7 +3,7 @@
 #SBATCH --nodes=1                    # Run all processes on a single node
 #SBATCH --ntasks=1                   # Run a single task
 #SBATCH --mem=30gb                   # Job memory request
-#SBATCH --partition=gpupart_p100          # GPU Partition
+#SBATCH --partition=gpupart_v100          # GPU Partition
 #SBATCH --gpus-per-node=2            #number of GPUS
 #SBATCH --time=1-23:00:00            # Time limit hrs:min:sec
 #SBATCH --output=/home/m1/23CS60R48/MTP/local_spatial/3D_local_output%j.log    # Standard output and error log
