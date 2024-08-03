f3a8fef243c5a27649150065f56374ef8a029964
nishantdhotre <dhotrenishant@gmail.com>
2024-08-03 18:56:07 +0530

    radiomic features added and used

--- job-new.sh
+++ job-new.sh
@@ -3,9 +3,9 @@
 #SBATCH --nodes=1                    # Run all processes on a single node
 #SBATCH --ntasks=1                   # Run a single task
 #SBATCH --mem=30gb                   # Job memory request
-#SBATCH --partition=gpupart_p100          # GPU Partition
+#SBATCH --partition=gpupart_1hour          # GPU Partition
 #SBATCH --gpus-per-node=2            #number of GPUS
-#SBATCH --time=1-23:00:00            # Time limit hrs:min:sec
+#SBATCH --time=0-00:50:00            # Time limit hrs:min:sec
 #SBATCH --output=/home/m1/23CS60R48/MTP/local_spatial/RF_t2_%j.log    # Standard output and error log
 
 #module load anaconda3
